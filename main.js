8d1021858f6299514a59b22b2f77e6434cc8c3d7
Laís Lima <lais@cosmobots.io>
2019-09-01 23:03:33 -0300

    icones da home ok

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 const menuButton = document.getElementById("toggle-menu")
 const menuOptions = document.getElementById("menu-options")
+const menu = document.getElementById("navbar");
+const sticky = menu.offsetTop;
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+  easing: 'easeInOutCubic',
+  // header: ['class="navbar"']
+});
+
+const fixMenu = () => {
+  if (window.pageYOffset > sticky) {
+    menu.classList.add("sticky");
+  } else {
+    menu.classList.remove("sticky");
+  }
+}
+window.onscroll = function() {fixMenu()};
+
+
 menuButton.addEventListener("click", (e) => {
   menuOptions.classList.toggle("open")
 })
